5cdda45bcf1222e762a4987ec901fb0d4c8a7de4
Brunine Einstein <84164381+Einstein201@users.noreply.github.com>
2023-08-07 23:10:41 -0400

    Add files via upload

---
+++ cpt34Einstein.py
@@ -0,0 +1,46 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Coding Challenge")
+Coding Challenge
+
+non= "EINSTEIN"
+... 
+>>> 
+... non= "EINSTEIN"
+... 
+>>> LAJ=20
+>>> print("nom mwe se",non, "mwen gen", LAJ )
+nom mwe se EINSTEIN mwen gen 20
+>>> A=50.9
+>>> B=3
+>>> ADD=A+B
+>>> print(ADD)
+53.9
+>>> Mult=A*B
+>>> print("Rezilta miltiplikasyon:", Mult)
+Rezilta miltiplikasyon: 152.7
+>>> Div=A/B
+>>> print("rezilta divizyon A ak B" Div)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("rezilta divizyon A ak B" ,Div)
+rezilta divizyon A ak B 16.966666666666665
+>>> print (Sous= A-B)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    print (Sous= A-B)
+TypeError: 'Sous' is an invalid keyword argument for print()
+>>> SOUS =A-B
+>>> print (SOUS)
+47.9
+>>> Y=False
+>>> var = "Hello"
+>>> Vr1=88
+>>> vr=99.99
+>>> type(Y)
+<class 'bool'>
+>>> type(var)
+<class 'str'>
+>>> type(Vr1)
+<class 'int'>
+>>> type(vr)
+<class 'float'>
